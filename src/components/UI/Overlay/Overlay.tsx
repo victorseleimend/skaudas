44c1b0a82e304378a3cf77bcff0bd30ab16ec920
victorseleimend <victorseleimend1@gmail.com>
2021-12-20 09:15:16 -0300

    Adaptação do overlay

--- src/components/UI/Overlay/Overlay.tsx
+++ src/components/UI/Overlay/Overlay.tsx
@@ -1,18 +1,26 @@
+import { FC } from "react";
 import { createPortal } from "react-dom";
 import useMobileMenu from "../../hooks/useMobileMenu";
 
 import classes from "./Overlay.module.css";
 
-const Overlay = () => {
-  const { setMobileMenuIsVisible } = useMobileMenu();
-
+const Overlay: FC<{ onClickHandler: () => void; isModal?: boolean }> = ({
+  onClickHandler,
+  isModal,
+}) => {
   return createPortal(
     <div
       role="none"
-      onClick={() => setMobileMenuIsVisible(false)}
+      style={{ zIndex: isModal ? 999 : 2 }}
+      onClick={onClickHandler}
       className={classes.overlay}
     />,
     document.getElementById("layer")!
   );
 };
+
+Overlay.defaultProps = {
+  isModal: false,
+};
+
 export default Overlay;
